7a523edca3a7879420e111d0f43c054cc7784ff8
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-03-08 15:56:16 -0300

    configuracion de rutas para usuarios

---
+++ grow/api/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router(); 
+const user = require('./users');
+
+
+router.use('/user', user)
+
+
+
+
+module.exports =router ; 
---
+++ grow/api/routes/users.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const User = require("../models");
+
+router.post("/register", (req, res) => {
+  User.create(req.body).then((user) => {
+    res.sendStatus(201);
+  });
+});
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.send(req.user);
+});
+router.post("/logout", (req, res) => {
+  req.logOut();
+  res.sendStatus(200);
+});
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  User.update(req.body, {
+    where: {
+      id
+    },
+    returning: true,
+    plain: true,
+  }).then((result) => {
+    const user = result[1];
+    res.status(201).json({
+      user,
+    });
+  });
+});
+
+router.get('/me',(req,res)=>{
+    if(!req.user) return res.sendStatus(401)
+    res.send(req.user); 
+}); 
+
+
+
+module.exports = router;
