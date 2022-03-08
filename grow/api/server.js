4c52ee905ecea9ebd57112c79f3b8c3152a5e1b3
fmelion <melionfederico@gmail.com>
2022-03-08 15:56:01 -0300

    configuracion de passport

--- grow/api/server.js
+++ grow/api/server.js
@@ -1,8 +1,89 @@
 const express = require('express');
+const http = require('http');
+const { User } = require('./models');
+
 const app = express();
 
-// algo nuevo de pruebba ...
+const db = require('./db');
 
-///esto es un comentario
+const bodyParser = require('./bodyParser');
+const routes = require('./routes');
+const morgan = require('morgan');
 
-//////esto es otro comentario mas
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('tiny'));
+
+app.use(cookieParser());
+
+app.use(
+  sessions({
+    secret: 'sativaANDco',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { _expires: 60000000000000 },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+
+    function (email, password, done) {
+      User.findOne({ where: { email } })
+        .then(user => {
+          if (!user) {
+            return done(null, false);
+          }
+
+          user.setHash(password, user.salt).then(hash => {
+            if (hash !== user.password) {
+              return done(null, false);
+            }
+
+            return done(null, user);
+          });
+        })
+        .catch(done);
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findByPk(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(done);
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(500).send(err);
+});
+
+app.use('/api', routes);
+
+const port = 3001;
+
+db.sync({ force: false }).then(() => {
+  app.listen(port, function () {
+    console.log(`Listening on port ${port}!`);
+  });
+});
