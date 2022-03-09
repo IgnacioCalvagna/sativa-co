2cb8cc49fd18e8966f7d6bddcbc4d55633fdcb0d
fmelion <melionfederico@gmail.com>
2022-03-09 00:10:35 -0300

    probando relaciones

--- grow/api/models/index.js
+++ grow/api/models/index.js
@@ -1,12 +1,30 @@
-const Address = require ("./Address")
-const CartItem = require ("./CartItem")
-const Category = require ('./Category')
-const OrederDetail = require ("./OrderDetail")
-const OrderItem = require ("./OrderItem")
-const PaymentDetail = require ("./PaymentDetail")
-const Product = require ("./Product")
-const ShoppingCart = require ("./ShoppingCart")
-const User = require ("./User")
-//Favourites.belongsTo(Users, { as: "idFav" });
+const Address = require('./Address');
+const CartItem = require('./CartItem');
+const Category = require('./Category');
+const OrderDetail = require('./OrderDetail');
+const OrderItem = require('./OrderItem');
+const PaymentDetail = require('./PaymentDetail');
+const Product = require('./Product');
+const ShoppingCart = require('./ShoppingCart');
+const User = require('./User');
 
-module.exports = { User, Product,Address,CartItem,Category,OrederDetail,OrderItem,PaymentDetail,ShoppingCart };
+ShoppingCart.belongsTo(User);
+User.hasOne(ShoppingCart);
+
+CartItem.belongsTo(ShoppingCart);
+ShoppingCart.hasMany(CartItem);
+
+//Product.hasMany(CartItem);
+CartItem.belongsTo(Product);
+
+module.exports = {
+  User,
+  Product,
+  Address,
+  CartItem,
+  Category,
+  OrderDetail,
+  OrderItem,
+  PaymentDetail,
+  ShoppingCart,
+};
