e3c09c631334fb0c321b9d29b2b40a5acf822c56
Hiratsuki-0 <hiratsukin@gmail.com>
2022-03-16 22:17:34 +0900

    order 2.0

--- grow/src/components/OrderHistorial.js
+++ grow/src/components/OrderHistorial.js
@@ -1,13 +1,13 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { Link } from "react-router-dom";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router";
+// import { Link } from "react-router-dom";
 import FakeOrders from "../utils/FakeOrders.js";
 import OrderItem from "../commons/OrderItem"
 
 import "../style/OrderHistorial.css";
 
-const OrderHistorial = (props) => {
+const OrderHistorial = () => {
     const datas = FakeOrders
   return (
     <div classname="">
@@ -15,6 +15,7 @@ const OrderHistorial = (props) => {
               <div className="order-title">Order Historial</div>
 
               <div className="">
+
               {datas.map((data, i) => (
                   <OrderItem data={data}/>
               ))}
@@ -25,3 +26,5 @@ const OrderHistorial = (props) => {
 };
 
 export default OrderHistorial;
+
+
